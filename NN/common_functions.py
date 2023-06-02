c37464d25a01ea0efdec62e8f64c09494361568f
Phil Short <shortphilip4@gmail.com>
2023-06-02 13:39:59 +0100

    commit operation

--- NN/common_functions.py
+++ NN/common_functions.py
@@ -3,7 +3,7 @@ import copy, math
 import matplotlib.pyplot as plt
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
-#Activation functions
+#Activation functions (g)
 
 #Sigmoid function
 def sigmoid(z):
@@ -13,91 +13,89 @@ def sigmoid(z):
     return g
 
 #relu function
-def relu(z)
+def relu(z):
 
-	g = np.max(0,z)
+	zeros = np.zeros(len(z))
+	g = np.maximum(zeros,z)
 
 	return g
 
+def softmax(z):  
+           
+    a = np.exp(z)/np.sum(np.exp(z))
+    
+    return a
+
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Cost function for logistic regression with regularisation
-def cost_log(X, y, w, b, lambda_ = 0, activation = "sigmoid"):
+def cost_log(X, y, w, b, g, lambda_ = 0):
 
-    m = X.shape[0]
-    z = np.dot(X,w) + b
+	m = X.shape[0]
+	z = np.dot(X,w) + b
 	
-	if act == "sigmoid":
-    	f_wb = sigmoid(z)
-	else if act == "relu":
-		f_wb = relu(z)
+	f_wb = g(z)
     
-    reg = (lambda_/(2*m)) * np.dot(w,w) #regularisation term 
+	reg = (lambda_/(2*m)) * np.dot(w,w) #regularisation term 
     
-    cost =  ((1/m) * np.sum( -y*np.log(f_wb) - (1-y)*np.log(1-f_wb) )) + reg
+	cost =  ((1/m) * np.sum( -y*np.log(f_wb) - (1-y)*np.log(1-f_wb) )) + reg
              
-    cost /= m
+	cost /= m
     
-    return cost
+	return cost
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Calculate gradient with regularisation
-def gradient(X, y, w, b, lambda_=0, activation="sigmoid"): 
+def gradient(X, y, w, b, g, lambda_=0): 
 
-    m = X.shape[0]
-    dj_dw = np.zeros((n,))                           
+	m = X.shape[0]
+	dj_dw = np.zeros((n,))                           
 
-    z = np.dot(X,w) + b
-   if act == "sigmoid":
-    	f_wb = sigmoid(z)
-	else if act == "relu":
-		f_wb = relu(z) 
+	z = np.dot(X,w) + b
+	f_wb = g(z)
     
-    reg = (lambda_/m) * np.sum(w) #regularisation term
+	reg = (lambda_/m) * np.sum(w) #regularisation term
                     
-    dj_dw = (1/m) * np.dot(f_wb - y, X) + reg
-    dj_db = (1/m) * np.sum(f_wb - y)
+	dj_dw = (1/m) * np.dot(f_wb - y, X) + reg
+	dj_db = (1/m) * np.sum(f_wb - y)
                                
         
-    return dj_db, dj_dw  
+	return dj_db, dj_dw  
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Perform gradient descent with logistic regression
-def gradient_descent(X, y, w_in, b_in, alpha, n, lambda_=0, activation="sigmoid"): 
+def gradient_descent(X, y, w_in, b_in, alpha, n, g, lambda_=0): 
 
-    J_history = []
-    w = copy.deepcopy(w_in)  #avoid modifying global w within function
-    b = b_in
+	J_history = []
+	w = copy.deepcopy(w_in)  #avoid modifying global w within function
+	b = b_in
     
-    for i in range(n):
-        # Calculate the gradient and update the parameters
-        dj_db, dj_dw = gradient(X, y, w, b, lambda_)   
+	for i in range(n):
+		# Calculate the gradient and update the parameters
+		dj_db, dj_dw = gradient(X, y, w, b, g, lambda_)   
 
-        # Update Parameters using w, b, alpha and gradient
-        w = w - alpha * dj_dw               
-        b = b - alpha * dj_db               
+		# Update Parameters using w, b, alpha and gradient
+		w = w - alpha * dj_dw               
+		b = b - alpha * dj_db               
       
-        # Save cost J at each iteration
-        if i<100000:      # prevent resource exhaustion 
-            J_history.append(cost_log(X, y, w, b, activation) )
+		# Save cost J at each iteration
+		if i<100000:      # prevent resource exhaustion 
+			J_history.append(cost_log(X, y, w, b, g, lambda_) )
 
-        # Print cost every at intervals 10 times or as many iterations if < 10
-        if i% math.ceil(n / 10) == 0:
-            print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
+		# Print cost every at intervals 10 times or as many iterations if < 10
+		if i% math.ceil(n / 10) == 0:
+			print(f"Iteration {i:4d}: Cost {J_history[-1]}   ")
         
-    return w, b, J_history
+		return w, b, J_history
 
 
 #------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #Computes layer of NN, equivalent to Dense function in Tensorflow
-def my_dense(a_in, W, b, activation="sigmoid"):
+def my_dense(a_in, W, b, g):
                                             
-    z = np.dot(np.transpose(W), a_in) + b
+	z = np.dot(np.transpose(W), a_in) + b
 	
-	   if act == "sigmoid":
-    	a_out = sigmoid(z)
-	else if act == "relu":
-		a_out = relu(z) 
+	a_out = g(z)
          
-    return(a_out)
+	return(a_out)
